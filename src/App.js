a0f8dab6d5526ba2ec02f2dece764c9fd2e2ab57
LEODUARTE99 <leonardotnduarte@hotmail.com>
2020-08-07 14:45:31 -0300

    Setup inicial do projeto

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Menu from './components/Menu'
+import dadosIniciais from './data/dados_iniciais.json'
+import BannerMain from './components/BannerMain'
+import Carousel from './components/Carousel'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div style={{backgroundColor:"#141414"}}>
+      <Menu />
+      
+      <BannerMain 
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"Vídeo preferido"}
+      />
+      {dadosIniciais.categorias.map(tema=>
+      <Carousel 
+        category={tema}
+        ignoreFirstVideo = {tema.videos.length>5}
+      />)}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Menu/components/ButtonLink/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function ButtonLink( props ){
+    // props => { className: " o que alguém vai passar ", href="/"}
+
+    return(
+        <a href={props.href} className={props.className}>
+            {props.children}
+        </a>
+
+    );
+}
+
+export default ButtonLink;
